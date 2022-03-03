ff25fcdb3a061274a47433e8ca7b31291d0d95c0
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-02 19:04:12 -0800

    processing html

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import './css/styles.css';
   src="https://code.jquery.com/jquery-3.5.1.min.js"
   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
   crossorigin="anonymous"></script>
-  <script type="text/javascript" src="bundle.js"></script>
   <title>Shape Tracker</title>
 </head>
