a65eb0dc09945518f6d769ed7eac46f4eadd100e
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-10 14:45:39 -0800

    green phase: pass create yearsLeft object with earthExtraYears equating close to 1

--- src/js/solarYearCalculator.js
+++ src/js/solarYearCalculator.js
@@ -16,12 +16,24 @@ export default class SolarAge {
     this.venusDeathClock = (this.earthAverageLifeSpan * .62) - (this.earth * .62);
     this.marsDeathClock = (this.earthAverageLifeSpan * 1.88) - (this.earth * 1.88);
     this.jupiterDeathClock = (this.earthAverageLifeSpan * 11.86) - (this.earth * 11.86);
+
+    this.earthExtraYears = (Math.sign(this.earthDeathClock) === -1) ? Math.abs(this.earthDeathClock) : this.earthDeathClock;
+    // this.mercuryExtraYears = (Math.sign(this.mercuryDeathClock) === -1) ? Math.abs(this.mercuryDeathClock) : this.mercuryDeathClock;
+    // this.venusExtraYears = (Math.sign(this.venusDeathClock) === -1) ? Math.abs(this.venusDeathClock) : this.venusDeathClock;
+    // this.marsExtraYears = (Math.sign(this.marsDeathClock) === -1) ? Math.abs(this.marsDeathClock) : this.marsDeathClock;
+    // this.jupiterExtraYears = (Math.sign(this.jupiterDeathClock) === -1) ? Math.abs(this.jupiterDeathClock) : this.jupiterDeathClock;
+
+
+    // this.earthExtraYears = this.earthDeathClock > 0 ? Math.abs(this.earthDeathClock) : this.earthDeathClock;
   }
 
-  eternalYears () {
-    // if (Math.sign(this.earthDeathClock) === -1) {
-      // const earthBonusYears = Math.abs(this.earthDeathClock);
-      // return earthBonusYears;
-    // }
-  }
+  // eternalYears () {
+    // this.earthExtraYears = this.earthDeathClock > 0 ? Math.abs(this.earthDeathClock) : this.earthDeathClock;
+  //   // this.earthExtraYears = (Math.sign(this.earthDeathClock) === -1) ? 
+  //   // Math.abs(this.earthDeathClock) : this.earthDeathClock;
+  //   // if (Math.sign(this.earthDeathClock) === -1) {
+  //   //   const earthBonusYears = Math.abs(this.earthDeathClock);
+  //   //   return earthBonusYears;
+  //   // }
+  // }
 }
