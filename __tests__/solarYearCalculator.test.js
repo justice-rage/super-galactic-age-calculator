82eff2a1fd3f36189953340c9a10e01238db7ca1
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-10 09:23:41 -0800

    red phase: create object containing earth

--- __tests__/solarYearCalculator.test.js
+++ __tests__/solarYearCalculator.test.js
@@ -2,9 +2,9 @@ import SolarAge from './../src/js/solarYearCalculator.js';
 
 describe('SolarAge', () => {
 
-  test('should succesfully create an empty age object', () => {
+  test('should succesfully create an age object with earth', () => {
     const age = new SolarAge();
-    // expect(triangle.side1).toEqual(2);
+    expect(age.earth).toEqual(1);
     // expect(triangle.side2).toEqual(4);
     // expect(triangle.side3).toEqual(5);
   });
