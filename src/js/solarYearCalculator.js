cce991fa8390486f483cb66e3c74475de6fe875d
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-10 09:56:37 -0800

    green phase: pass create object with earth equating to 1

--- src/js/solarYearCalculator.js
+++ src/js/solarYearCalculator.js
@@ -1,8 +1,8 @@
 'use strict';
 
 export default class SolarAge {
-  constructor () {
-    this.earth = 1;
+  constructor (earth) {
+    this.earth = earth;
     this.mercury = this.earth * .24;
     this.venus = this.earth * .62;
     this.mars = this.earth * 1.88;
