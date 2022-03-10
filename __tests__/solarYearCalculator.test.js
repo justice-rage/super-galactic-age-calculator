3ebe37c81ac9ca6e8b9f4243eaaeff632de85219
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-10 09:37:25 -0800

    red phase: create object containing venus

--- __tests__/solarYearCalculator.test.js
+++ __tests__/solarYearCalculator.test.js
@@ -2,10 +2,10 @@ import SolarAge from './../src/js/solarYearCalculator.js';
 
 describe('SolarAge', () => {
 
-  test('should succesfully create an age object with mercury', () => {
+  test('should succesfully create an age object with venus', () => {
     const age = new SolarAge();
     expect(age.earth).toEqual(1);
     expect(age.mercury).toEqual(.24);
-    // expect(triangle.side3).toEqual(5);
+    expect(age.venus).toEqual(.62);
   });
 });
