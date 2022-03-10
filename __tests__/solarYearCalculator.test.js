1397de23809965371b2a17265e6b9a852d17cdfe
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-10 09:44:11 -0800

    red phase: create object with jupiter

--- __tests__/solarYearCalculator.test.js
+++ __tests__/solarYearCalculator.test.js
@@ -2,11 +2,12 @@ import SolarAge from './../src/js/solarYearCalculator.js';
 
 describe('SolarAge', () => {
 
-  test('should succesfully create an age object with mars', () => {
+  test('should succesfully create an age object with jupiter', () => {
     const age = new SolarAge();
     expect(age.earth).toEqual(1);
     expect(age.mercury).toEqual(.24);
     expect(age.venus).toEqual(.62);
     expect(age.mars).toEqual(1.88);
+    expect(age.jupiter).toEqual(11.86);
   });
 });
