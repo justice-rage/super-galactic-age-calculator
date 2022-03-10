547c0f00a33b8036a3082e53b576a97575944021
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-10 09:45:45 -0800

    green phase: pass create object with jupiter

--- src/js/solarYearCalculator.js
+++ src/js/solarYearCalculator.js
@@ -6,5 +6,6 @@ export default class SolarAge {
     this.mercury = this.earth * .24;
     this.venus = this.earth * .62;
     this.mars = this.earth * 1.88;
+    this.jupiter = this.earth * 11.86;
   }
 }
